1313767f4c92dcdf3117b8f106d803454afab998
surajDongre-16 <surajdongre8@gmail.com>
2023-05-23 12:56:41 +0530

    login and signup ui created

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent={"center"}
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Text
+          textAlign={"center"}
+          fontSize={"4xl"}
+          fontFamily={"Work sans"}
+          color={"black"}
+        >
+          Chat by Suraj
+        </Text>
+      </Box>
+      <Box p={4} bg={"white"} borderRadius={"lg"} borderWidth={"1px"} w="100%">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel><Login/></TabPanel>
+            <TabPanel><Signup/></TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
