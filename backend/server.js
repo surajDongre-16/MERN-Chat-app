ae0be59001b3b1d1028bd343e1d632bff1fff4fc
surajDongre-16 <surajdongre8@gmail.com>
2023-05-23 22:25:59 +0530

    database connected to app

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const chats = require("./data/data");
+const connectDB = require("./config/db");
 
 dotenv.config();
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
