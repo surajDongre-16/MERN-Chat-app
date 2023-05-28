887fd6aca57e4b581b4a1da237062ae556fecf8f
surajDongre-16 <surajdongre8@gmail.com>
2023-05-28 14:50:09 +0530

    work in progress

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,23 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { ChatState } from "../Context/ChatProvider";
+import { Box } from "@chakra-ui/react";
+import SiderDrawer from "../components/miscellaneous/SiderDrawer";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
 
 const ChatPage = () => {
-  const [chats, setChats] = useState([]);
-
-  const fetchChats = async () => {
-    const { data } = await axios.get("/api/");
-    setChats(data);
-  };
-
-  useEffect(() => {
-    // fetchChats();
-  }, []);
-
+  const { user } = ChatState();
   return (
-    <div>
-      {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-      ))}
+    <div style={{ width: "100%" }}>
+      {user && <SiderDrawer />}
+      <Box>
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
     </div>
   );
 };
